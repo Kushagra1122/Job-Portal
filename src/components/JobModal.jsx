6f65eb371ddf31ace2571252b98bbf31a3657f3f
Kushagra1122 <kushagratiwari.231ee128@nitk.edu.in>
2024-10-21 13:47:30 +0530

    deploying

--- src/components/JobModal.jsx
+++ src/components/JobModal.jsx
@@ -1,16 +1,18 @@
 
 import React from "react";
+import { useTheme } from "../context/Theme";
 
 const JobModal = ({ job, onClose }) => {
+  const [isDark] = useTheme();
   if (!job) return null; // If no job is selected, return null
 
   return (
     <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center p-4 items-center">
       <div className="bg-white p-6 rounded-lg max-w-md w-full">
-        <h2 className="text-2xl font-bold mb-2">{job.title}</h2>
+        <h2 className={`text-2xl font-bold mb-2 ${isDark?"text-white":""}`}>{job.title}</h2>
         <p className="text-gray-600 mb-4">{job.company}</p>
         <p className="text-gray-500 mb-4">{job.location}</p>
-        <p className="mb-4">{job.description}</p>
+        <p className={`mb-4${isDark?"text-white":""}`}>{job.description}</p>
         <div className="flex justify-between pr-2">
           <button className="bg-blue-500 text-white py-2 px-4 rounded mb-2">
             Apply Now
